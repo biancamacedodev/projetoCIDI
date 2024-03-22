0eb80c00c55aea7f370c1f5af5da2688627ea279
biancamacedodev <biancamcedo97@gmail.com>
2024-03-22 16:28:30 -0300

    003 - Refatoraçao do codigo e ajustes de media queries

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const navbarToggler = document.querySelector('.navbar-toggler');
+        const navbarCollapse = document.querySelector('.navbar-collapse');
+
+        navbarToggler.addEventListener('click', function() {
+            navbarCollapse.classList.toggle('show');
+        });
+    });
